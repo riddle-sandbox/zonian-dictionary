09b340d51e22bc4cf43b9c9a11bc14f72e2056bd
riddle <xxx@sample.com>
2026-09-05 20:37:12 +0900

    ポスト内容修正

--- detail.js
+++ detail.js
@@ -30,9 +30,9 @@ detailExample.textContent = exampleSentence;
 
 if (isEncouragementTerm) {
 	const postLink = document.createElement("a");
-	const postText = "【ARG】『ゾナ語』を解いて、ゾナ語を全てを観測した！\n#ゾナ語謎";
 	const publicSiteUrl = "https://riddle-sandbox.github.io/zonian-dictionary/";
-	postLink.href = `https://twitter.com/intent/tweet?text=${encodeURIComponent(postText)}&url=${encodeURIComponent(publicSiteUrl)}`;
+	const postText = `【ARG】『ゾナ語』を解いて、ゾナ語をマスターした！\n#ゾナ語謎\n${publicSiteUrl}`;
+	postLink.href = `https://twitter.com/intent/tweet?text=${encodeURIComponent(postText)}`;
 	postLink.textContent = "クリアポストをXに投稿する";
 	postLink.className = "button primary post-button";
 	postLink.target = "_blank";
